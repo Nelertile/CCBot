9815e064298777af41ca1a2eeaa2d6b49651db7e
NotToxic <47497223+NotToxicDev@users.noreply.github.com>
2021-03-22 17:16:15 +0100

    test

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
-const config = require('./config.json')
+require('dotenv').config()
+
 
 client.on('ready', () => {
     console.log('The client is ready!')
 })
 
-client.login(config.token)
+client.login(token)
